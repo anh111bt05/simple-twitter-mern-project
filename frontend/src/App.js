e3a69caddd7cc010dcfccf7939dbcf3b07ecf955
anh_cxview <anh.nguyenquoc@meditech.vn>
2021-07-28 09:43:54 +0700

    Frontend-# 2 Add feature current user login  navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,44 @@ import Register from "./components/Register";
 import "./index.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AppReducer from "./reducers/AppReducer";
-import { useReducer } from "react";
+import {useEffect, useCallback, useReducer } from "react";
 import AppContext from "./components/AppContext";
+import axios from "axios";
 
 function App() {
   const initialState = { user: null, post: [] };
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  //check current khi ma user reload lai app ma ko cap nhat duoc ten trong Appcontext (quay ve default la null)
+  const checkCurrentUser = useCallback(async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const options = {
+        method: "get",
+        url: "/api/v1/auth/",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await axios(options);
+      if (response.data.data.user) {
+        const { userName } = response.data.data.user;
+        dispatch({ type: "CURRENT_USER", payload: { userName } });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    checkCurrentUser()
+    
+  }, [checkCurrentUser])
+
   return (
     <Router>
       {/* Provider tiep nhan thong tin truy cap vao  */}
-      <AppContext.Provider value={{state, dispatch}}>
+      <AppContext.Provider value={{ state, dispatch }}>
         <div className="container">
           <Header />
           <Switch>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../css/Header.css";
 import { Link } from "react-router-dom";
+import AppContext from "./AppContext";
 
 export default function Header() {
+  const { state, dispatch } = useContext(AppContext);
+  const { user } = state;
+  const signOut = () => {
+    localStorage.removeItem("token");
+    dispatch({ type: "CURRENT_USER", payload: null });
+  };
   return (
     <header className="header">
       <h1 className="logo">
@@ -10,20 +17,27 @@ export default function Header() {
       </h1>
       <nav>
         <ul className="main-nav">
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <span href="" classNameName="user-name">
-              Hello, anh nguyen
-            </span>
-          </li>
-          <li>
-            <Link to="/">Logout</Link>
-          </li>
+          {user ? (
+            <>
+              <li>
+                <span href="" className="user-name">
+                  Hello, {user.userName}
+                </span>
+              </li>
+              <li onClick={() => signOut()}>
+                <a>Logout</a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
